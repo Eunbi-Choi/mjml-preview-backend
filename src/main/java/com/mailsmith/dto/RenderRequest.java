6c3fe6016691838756213909efd564ecfdb184b5
Eunbi <eunbi9533@naver.com>
2025-12-21 21:54:33 +0900

    feat: MJML 렌더링 DTO 작성

---
+++ src/main/java/com/mailsmith/dto/RenderRequest.java
@@ -0,0 +1,25 @@
+package com.mailsmith.dto;
+
+import java.util.Map;
+
+public class RenderRequest {
+    private String mjml;
+    private Map<String, Object> variables;
+
+    public String getMjml() {
+        return mjml;
+    }
+
+    public void setMjml(String mjml) {
+        this.mjml = mjml;
+    }
+
+    public Map<String, Object> getVariables() {
+        return variables;
+    }
+
+    public void setVariables(Map<String, Object> variables) {
+        this.variables = variables;
+    }
+}
+
