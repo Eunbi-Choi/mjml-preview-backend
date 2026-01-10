80180d1fd498246ac9231fc72ac8bac0dc99882b
Eunbi <eunbi9533@naver.com>
2026-01-11 02:05:53 +0900

    feat: add name

--- src/main/java/com/templify/controller/HealthCheckController.java
+++ src/main/java/com/templify/controller/HealthCheckController.java
@@ -28,6 +28,7 @@ public class HealthCheckController {
         responseData.put("serverAddress", serverAddress);
         responseData.put("serverPort", serverPort);
         responseData.put("env", env);
+        responseData.put("name", "최은비");
 
         return ResponseEntity.ok(responseData);
     }
